db1002d5dffa57b0232a2e0f1d7878976ae48eec
467764098@qq.com <ningboyue123>
2024-11-13 02:48:58 +0800

    admin--添加用户模块

--- admin/src/views/MainBox.vue
+++ admin/src/views/MainBox.vue
@@ -18,3 +18,10 @@
 import SideMenu from '../components/mainbox/SideMenu.vue'
 import TopHeader from '../components/mainbox/TopHeader.vue'
 </script>
+
+<style lang="scss" scoped>
+.el-main{
+    overflow: auto;
+    height: calc(100vh - 60px);
+}
+</style>
