e732ef441c320dc895b7688cf85d1046954ef66f
467764098@qq.com <ningboyue123>
2024-11-15 00:06:26 +0800

    admin--创建新闻模块

---
+++ admin/src/components/editor/Editor.vue
@@ -0,0 +1,30 @@
+<!--
+ * @作者: NingBY
+ * @Date: 2024-11-14 21:00:16
+-->
+<template>
+    <div id="myeditor">
+        
+    </div>
+</template>
+
+<script setup>
+import { onMounted, defineEmits } from 'vue'
+import E from "wangeditor"
+const emit = defineEmits(["event"])
+onMounted(() => {
+    const editor = new E('#myeditor')
+    editor.create()
+
+    editor.config.onchange  = function (newHtml) {
+        // console.log("change 之后最新的 html", newHtml)
+
+        emit("event", newHtml)
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
--- admin/src/router/config.js
+++ admin/src/router/config.js
@@ -3,14 +3,14 @@
  * @Date: 2024-11-10 18:02:16
  */
 import Home from "@/views/home/Home.vue"
-import Center from '../views/center/Center.vue'
-import UserAdd from '../views/user-manage/UserAdd.vue'
-import UserList  from '../views/user-manage/UserList.vue'
-import NewsAdd from '../views/news-manage/NewsAdd.vue'
-import NewsList from '../views/news-manage/NewsList.vue'
-import ProductAdd from '../views/product-manage/ProductAdd.vue'
-import ProductList from '../views/product-manage/ProductList.vue'
-import NotFound from '../views/notfound/NotFound.vue'
+import Center from '@/views/center/Center.vue'
+import UserAdd from '@/views/user-manage/UserAdd.vue'
+import UserList  from '@/views/user-manage/UserList.vue'
+import NewsAdd from '@/views/news-manage/NewsAdd.vue'
+import NewsList from '@/views/news-manage/NewsList.vue'
+import ProductAdd from '@/views/product-manage/ProductAdd.vue'
+import ProductList from '@/views/product-manage/ProductList.vue'
+import NotFound from '@/views/notfound/NotFound.vue'
 const  routes = [
     {
         path:"/index",
