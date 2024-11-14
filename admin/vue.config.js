c7f78b1a706f97d86e48e77279a1b4afd05a1194
467764098@qq.com <ningboyue123>
2024-11-15 00:07:26 +0800

    提示警告：__VUE_PROD_HYDRATION_MISMATCH_DETAILS__

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,7 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const { DefinePlugin } = require('webpack')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  configureWebpack: {
+    plugins: [
+      new DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+      })
+    ]
+  },
   devServer: {
     proxy: {
       '/adminapi': {
